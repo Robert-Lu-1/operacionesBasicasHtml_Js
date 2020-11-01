8798fdf898b1a40b2fa0ffa8d1887be600e82308
Robert L} <robertmorales181@gmail.com>
2020-11-01 10:45:24 -0600

    inicio del repositorio

---
+++ division/myscript.js
@@ -0,0 +1,32 @@
+
+function myFunction() {  
+  //variables
+  var x,y,res,text;  
+  //Asignar numeros ingresados a variables
+  x = document.getElementById("num1").value;  
+  y = document.getElementById("num2").value;  
+
+  //Si los campos estan vacios se muestra un mensaje
+  if (isNaN(parseFloat(document.getElementById('num1').value))) {
+    document.getElementById('resultado').innerHTML="<span style='color: red;'>Por favor, escriba un número </span>";
+    document.getElementById("num1").innerText = "0";
+    document.getElementById("num1").focus();
+    } else if (isNaN(parseFloat(document.getElementById('num2').value))) {
+    document.getElementById('resultado').innerHTML="<span style='color: red;'>Por favor, escriba un número </span>";
+    document.getElementById("num2").innerText = "0";
+    document.getElementById("num2").focus();
+}
+else {
+  //si se ingresan datos se hace la operacion
+  if (parseFloat(y)==0) {  
+    text = "No se puede dividir entre 0";  
+  } else {  
+    //si no ponemos parseFloat concatenaría x con y  
+    res=parseFloat(x) / parseFloat(y);  
+    text= res;  
+  }  
+  document.getElementById("resultado").innerHTML = text;  
+}
+
+  
+}  
